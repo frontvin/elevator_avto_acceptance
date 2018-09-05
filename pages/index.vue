b3fb1b143fe3f0681544291334cfbc737d55fba4
bvsbvs <bvs0618@gmail.com>
2018-09-05 15:44:55 +0300

    fixed problem with element.io

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,36 @@
 <template>
-    <div id="SignUp">    
-      <div id="signCont">
-        <!-- <input v-model="login" placeholder="Enter your login"> -->
-        <el-input
-          placeholder="Please input login"
-          v-model="login"
-          clearable>
-        </el-input>
-        <el-input
-          placeholder="Please input pass"
-          v-model="pass"
-          clearable>
-        </el-input>
-        <!-- <input v-model="pass" placeholder="Enter your password"> -->
-        <button v-on:click="signUp"> Let me in. </button>
-      </div>
-  </div> 
+  <el-form ref="auth" :model="auth">
+    <el-form-item label="login">
+      <el-input v-model="auth.login"></el-input>
+    </el-form-item>
+    <el-form-item label="Pass">
+      <el-input v-model="auth.pass"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="signUp">Submit</el-button>
+    </el-form-item>
+  </el-form> 
 </template>
 
 <script>
-// import LoginForm from '~/components/LoginForm.vue'
+
 
 export default {
     data() { 
       return {
-        login: '',
-        pass: ''
+        auth: {
+          login: '',
+          pass: ''
+        }
       }
     },
     methods: {
       signUp: function (){
-        localStorage.setItem('login', this.login);
-        localStorage.setItem('pass', this.pass);
-          if(this.login=='user' && this.pass=='user'){
+        localStorage.setItem('login', this.auth.login);
+        localStorage.setItem('pass', this.auth.pass);
+          if(this.auth.login=='user' && this.auth.pass=='user'){
             this.$router.push('/registrations');
-          } else if(this.login=='' || this.pass==''){
+          } else if(this.auth.login=='' || this.auth.pass==''){
             alert('You not enter login or password');
           } else {
             alert('You entered wrong login or password');
--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'node_modules/element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale/lang/en'
 
 export default () => {
   Vue.use(Element, { locale })
