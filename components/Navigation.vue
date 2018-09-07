e8d002966d1b7271e6bdb646ff93cd47728dd2c0
bvsbvs <bvs0618@gmail.com>
2018-09-07 18:47:28 +0300

    Problem with SCSS compilation was fixed. Start work with navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+<el-menu class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+  <el-menu-item index="1">Processing Center</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">Workspace</template>
+    <el-menu-item index="2-1">item one</el-menu-item>
+    <el-menu-item index="2-2">item two</el-menu-item>
+    <el-menu-item index="2-3">item three</el-menu-item>
+  </el-submenu>
+    <el-menu-item index="4">Orders</el-menu-item>
+</el-menu>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+@import url("//unpkg.com/element-ui@2.4.6/lib/theme-chalk/index.css");
+
+</style>
--- pages/registrations.vue
+++ pages/registrations.vue
@@ -1,3 +1,18 @@
 <template>
+    <div id='someid'>
+    <navigation/>
     <h1>Registration page</h1>
+    </div>
 </template>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+export default {
+    components:{
+        Navigation
+    }
+    
+}
+
+</script>
+
